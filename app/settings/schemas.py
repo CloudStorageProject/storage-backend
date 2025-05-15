6c0bfc557a242413c803f417cafd2ceb31988ed2
John Tomas <lsokhat06@gmail.com>
2025-05-16 01:15:52 +0300

    Security measure to check old password when changing credentials

--- app/settings/schemas.py
+++ app/settings/schemas.py
@@ -3,7 +3,9 @@ from pydantic import BaseModel, Field
 
 class UsernamePatch(BaseModel):
     username: str = Field(..., min_length=4, max_length=20, pattern=r'^[a-zA-Z0-9]+$')
+    old_password: str
 
 
 class PasswordPatch(BaseModel):
     password: str = Field(..., min_length=8, max_length=128)
+    old_password: str
